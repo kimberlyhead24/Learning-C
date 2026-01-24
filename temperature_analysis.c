6b4d00ca079cb18fce04010a2af692b8a818fef2
Kimberly Head <sara24rhodes@gmail.com>
2026-01-24 09:53:06 -0600

    Update temperature_analysis.c
    refactored for optimization using a single for loop, instead of multiple

--- temperature_analysis.c
+++ temperature_analysis.c
@@ -6,25 +6,21 @@ int main() {
     // DO NOT MODIFY ABOVE THIS LINE
     // TODO: Calculate average temperature
     int total = 0;
-    for (int i = 0; i < 6; i++) {
-        total += temperatures[i];
-    }
-    float average = (float)total / 6;
-
-    // TODO: Find highest temperature
-    // TODO: Find lowest temperature
     int maximum = temperatures[0];
     int minimum = temperatures[0];
-    for (int i = 1; i < 6; i++) {
+    for (int i = 0; i < 6; i++) {
+        total += temperatures[i];
+
         if (temperatures[i] < minimum) {
              minimum = temperatures[i];
         }
+
         if (temperatures[i] > maximum) {
             maximum = temperatures[i];
         }
     }
+    float average = (float)total / 6;
 
-    // TODO: Print results
     printf("Average Temperature: %.1f degrees\n", average);
     printf("Highest Temperature: %d degrees\n", maximum);
     printf("Lowest Temperature: %d degrees\n", minimum);
